861b7bcd40685e51e1007d3f58f2b087e4eec490
arman <arman.sahakyan.dev@gmail.com>
2017-03-21 16:03:44 +0400

    :lollipop: Added ConvertableList task.

--- Algorithms/Algorithms/Lists/ListSeries.cs
+++ Algorithms/Algorithms/Lists/ListSeries.cs
@@ -9,6 +9,35 @@ namespace Algorithms
     class ListSeries
     {
         /// <summary>
+        /// check whether an element occurs in a list.
+        /// </summary>
+        public void CheckElement()
+        {
+            while (true)
+            {
+                List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                int key = int.Parse(Console.ReadLine());
+
+                int i = 0;
+                while (i < list.Count)
+                {
+                    if (key == list[i])
+                    {
+                        Console.WriteLine(true.ToString());
+                        break;
+                    }
+                    i++;
+                }
+
+
+                if (i == list.Count)
+                {
+                    Console.WriteLine(false.ToString());
+                }
+            }
+        }
+
+        /// <summary>
         /// Rotate a list by k elements
         /// </summary>
         public void RotateElements()
@@ -28,5 +57,33 @@ namespace Algorithms
                 Console.WriteLine(item);
             }
         }
-    }
+
+        public void ConvertableList()
+        {
+            List<string> list = new List<string>() { "b1", "b2", "1b3", "1b23" };
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                string key = "";
+                char[] arr = new char[list[i].Length];
+
+                for (int j = 0; j < list[i].Length; j++)
+                {
+                    int number;
+                    string str = (list[i])[j].ToString();
+                    arr[j] = ((list[i])[j]);
+
+                    bool flag = int.TryParse((list[i])[j].ToString(), out number);
+                    if (flag == true)
+                    {
+                        key += number.ToString();
+                        if ((list[i])[j] == arr[arr.Length - 1])
+                        {
+                            Console.WriteLine(Convert.ToInt32(key));
+                        }
+                    }
+                }
+            }
+        }
+        }
 }
