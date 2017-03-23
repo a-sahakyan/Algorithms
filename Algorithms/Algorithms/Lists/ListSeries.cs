84d02d0651758019f8c4e520bbccda61d42efb3a
arman <arman.sahakyan.dev@gmail.com>
2017-03-23 21:48:36 +0400

    :pig: Optimized PigLatin.

--- Algorithms/Algorithms/Lists/ListSeries.cs
+++ Algorithms/Algorithms/Lists/ListSeries.cs
@@ -137,7 +137,7 @@ namespace Algorithms
         /// </summary>
         public void PigLatin()
         {
-            List<string> list = new List<string>() { "smmmile", "pig", "latin", "eat", "happy", "duck", "too" };
+            List<string> list = new List<string>() { "smile", "pig", "latin", "eat", "happy", "duck", "too" };
 
             for (int i = 0; i < list.Count; i++)
             {
@@ -152,31 +152,21 @@ namespace Algorithms
                 {
                     if ((listChar[k] == 'a' && flag == true) || (listChar[k] == 'e' && flag == true) || (listChar[k] == 'i' && flag == true) || (listChar[k] == 'o' && flag == true) || (listChar[k] == 'u' && flag == true) || (listChar[k] == 'y' && flag == true))
                     {
-                        if (listChar[0] == 'a' || listChar[0] == 'e' || listChar[0] == 'i' || listChar[0] == 'i' || listChar[0] == 'o' || listChar[0] == 'u' || listChar[0] == 'y')
+                        if (k == 0)
                         {
-                            Console.WriteLine($"{list[i]}" + "way");
+                            Console.WriteLine($"{list[i]}way");
                             break;
                         }
                         else
                         {
-                            int index = k;
-                            List<int> allIndexValues = new List<int>() { };
-
-                            while (index-- > 0)
+                            int index = 0;
+                            while (index < k)
                             {
-                                allIndexValues.Add(index);
-                            }
-
-                            for (int y = allIndexValues.Count - 1; y >= 0; y--)
-                            {
-                                listChar.Add(listChar[allIndexValues[y]]);
-                            }
-
-                            for (int l = 0; l < k; l++)
-                            {
+                                listChar.Add(listChar[0]);
                                 listChar.RemoveAt(0);
-                            }
+                                index++;
 
+                            }
                             flag = false;
                         }
                     }
@@ -187,7 +177,7 @@ namespace Algorithms
                         {
                             if (j == listChar.Count - 1)
                             {
-                                Console.Write(listChar[j] + "ay");
+                                Console.Write($"{listChar[j]}ay");
                             }
                             else
                             {
@@ -199,6 +189,5 @@ namespace Algorithms
                 }
             }
         }
-
     }
 }
