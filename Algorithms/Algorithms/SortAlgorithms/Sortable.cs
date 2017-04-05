23654879f050f8e49bd66f51c7e6050a8918c6f5
arman <arman.sahakyan.dev@gmail.com>
2017-04-05 21:49:14 +0400

    Added Selection sort algorithm.Optimized Bubble sort.

--- Algorithms/Algorithms/SortAlgorithms/Sortable.cs
+++ Algorithms/Algorithms/SortAlgorithms/Sortable.cs
@@ -8,20 +8,53 @@ namespace Algorithms
 {
    static class Sortable
     {
+        /// <summary>
+        /// Repeatedly compare neighbor pairs and swap if necessary.
+        /// </summary>
         public static void BubbleSort()
         {
             int[] arr = new int[] { 2, 5, 6, 8, 8, 1, 4, 7, 9, 3 };
 
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < arr.Length; j++) 
+                for (int j = i + 1; j < arr.Length; j++)
                 {
-                    if (arr[i] < arr[j])   //arr[i] > arr[j] to sort from large to small
+                    if (arr[i] > arr[j])   //arr[i] < arr[j] to sort from large to small
                     {
                         int temp = arr[i];
                         arr[i] = arr[j];
                         arr[j] = temp;
                     }
+                }
+            }
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /// <summary>
+        /// Repeatedly pick the smallest element to append to the result.
+        /// </summary>
+        public static void SelectionSort()
+        {
+            int[] arr = new int[] { 2, 5, 6, 8, 8, 1, 4, 7, 9, 3 };
+            int pos_min = 0, temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                pos_min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    pos_min = arr[j] < arr[pos_min] ? j : pos_min;
+                }
+
+                if (pos_min != i)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[pos_min];
+                    arr[pos_min] = temp;
                 }
             }
 
