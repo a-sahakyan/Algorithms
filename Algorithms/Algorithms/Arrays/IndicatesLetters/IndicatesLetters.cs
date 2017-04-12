6669317df0504f1a2c66195b14da4558ec4c8f15
arman <arman.sahakyan.dev@gmail.com>
2017-04-12 16:44:38 +0400

    :globe_with_meridians: Added Binary Search algorithm.Added array task.

---
+++ Algorithms/Algorithms/Arrays/IndicatesLetters/IndicatesLetters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Arrays.IndicatesLetters
+{
+    class IndicatesLetters
+    {
+        /// <summary>
+        /// creates an array containing all Latin letters. The user inputs a word from the console and as result the program prints to the console the indices of the letters from the word.
+        /// </summary>
+        public void GetIndexes()
+        {
+            //string[] letters = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "x", "y", "z" };
+            string[] letters = new string[26];
+            int index = 0;
+            for (int i = 97; i <= 122; i++)
+            {
+                letters[index++] = ((char)i).ToString();
+            }
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (word[i].ToString() == letters[j])
+                    {
+                        Console.Write($"{j} ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Algorithms/SortAlgorithms/BinarySearcher/BinarySearcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.SortAlgorithms.BinarySearcher
+{
+    static class BinarySearcher
+    {
+        //Binary search is a fast search algorithm with run-time complexity of Ο(log n). 
+        //This search algorithm works on the principle of divide and conquer. 
+        //For this algorithm to work properly, the data collection should be in the sorted form.
+        /// <summary>
+        /// Binary Search
+        /// </summary>
+        /// <param name="a">Sorted ascending array of type int []</param>
+        /// <param name="x">The desired element.</param>
+        private static int? BinarySearch(int[] a, int x)
+        {
+            // Проверить, имеет ли смыл вообще выполнять поиск:
+            // - если длина массива равна нулю - искать нечего;
+            // - если искомый элемент меньше первого элемента массива, значит, его в массиве нет;
+            // - если искомый элемент больше последнего элемента массива, значит, его в массиве нет.
+            if ((a.Length == 0) || (x < a[0]) || (x > a[a.Length - 1]))
+                return null;
+
+            // Приступить к поиску.
+            // Номер первого элемента в массиве.
+            int first = 0;
+            // Номер элемента массива, СЛЕДУЮЩЕГО за последним
+            int last = a.Length;
+
+            // Если просматриваемый участок не пуст, first < last
+            while (first < last)
+            {
+                int mid = first + (last - first) / 2;
+
+                if (x <= a[mid])
+                    last = mid;
+                else
+                    first = mid + 1;
+            }
+
+            // Теперь last может указывать на искомый элемент массива.
+            if (a[last] == x)
+                return last;
+            else
+                return null;
+        }
+    }
+}
