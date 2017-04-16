1fb2187f8d242d835491520737f70a6ca9284759
arman <arman.sahakyan.dev@gmail.com>
2017-04-17 01:47:39 +0400

    Added Fibonacci task with recursion.

---
+++ Algorithms/Algorithms/Recursion/Fibonacci/Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Recursion.Fibonacci
+{
+    class Fibonacci
+    {
+        public static long Fib(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
