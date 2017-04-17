f6bfc056a7620cda832b1bf4785ecb5bc9396012
arman <arman.sahakyan.dev@gmail.com>
2017-04-18 00:21:42 +0400

    Added recursion task.

---
+++ Algorithms/Algorithms/Recursion/FromAToB2/FromAToB2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Recursion.FromAToB2
+{
+    class FromAToB2
+    {
+        public string FATB(int a, int b)
+        {
+            if (a == b)
+            {
+                return $"{a}";
+            }
+
+            return FATB(a, b - 1) + " " + b;
+        }
+    }
+}
