3831c3fc767d760d3bcaaaa40252c47c7c94cf62
arman <arman.sahakyan.dev@gmail.com>
2017-04-18 00:14:51 +0400

    Added sum task with recursion.

---
+++ Algorithms/Algorithms/Recursion/Sum2/Sum2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Recursion.Sum2
+{
+    class Sum2
+    {
+        public int RecSum(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n + RecSum(n - 1);
+        }
+    }
+}
