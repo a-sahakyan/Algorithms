90884fc167bb589a4e30af9331cfb1b40898be99
arman <arman.sahakyan.dev@gmail.com>
2017-04-18 00:18:39 +0400

    Added recursion task.

---
+++ Algorithms/Algorithms/Recursion/FromOneToN2/FromOneToN2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Recursion.FromOneToN2
+{
+    class FromOneToN2
+    {
+        /// <summary>
+        /// prints from 1 to n numbers
+        /// </summary>
+        public void GetFromOneToN(int n)
+        {
+            if (n != 1)
+            {
+                GetFromOneToN(n - 1);
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
