9a80f33dc48efa3b894ec1a4f54d7cb876a897c7
arman <arman.sahakyan.dev@gmail.com>
2017-04-18 00:24:08 +0400

    Added ConvertToBinary task with recursion.

---
+++ Algorithms/Algorithms/Recursion/ConvertToBinary/ConvertToBinary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Recursion.ConvertToBinary
+{
+    class ConvertToBinary
+    {
+        /// <summary>
+        /// converts a decimal number to binary one.
+        /// </summary>
+        public void Convert(int n)
+        {
+            int temp = n % 2;
+
+            if (n >= 2)
+            {
+                Convert(n / 2);
+            }
+
+            Console.Write(temp);
+        }
+    }
+}
