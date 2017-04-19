5c23a87002734b3d9d778c63d5f297301cf6604d
arman <arman.sahakyan.dev@gmail.com>
2017-04-20 00:20:46 +0400

    Added StringsSum task.Updated package.config.

---
+++ Algorithms/Algorithms/Arithmetical/StringsSum/StringsSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Arithmetical.StringsSum
+{
+    class StringsSum
+    {
+        /// <summary>
+        /// Given a sequence of positive integer numbers given as string of numbers separated by a space.
+        /// <para>The program,calculates their sum.</para>
+        /// </summary>
+        public int GetSum(string numbers)
+        {
+            string[] arr = numbers.Split(' ');
+            int num;
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isConverted = int.TryParse(arr[i], out num);
+                if (isConverted)
+                {
+                    sum += num;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
