61b959ca84928b28f2c4ea074cefe4b2fdfa9cdf
arman <arman.sahakyan.dev@gmail.com>
2017-04-22 22:55:16 +0400

    Changed path of ReverseString and StringSum folders.

--- Algorithms/Algorithms/Strings/ReverseString/ReverseString.cs
+++ Algorithms/Algorithms/Strings/ReverseString/ReverseString.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algorithms.Arithmetical.ReverseString
+namespace Algorithms.Strings.ReverseString
 {
     class ReverseString
     {
--- Algorithms/Algorithms/Strings/StringsSum/StringsSum.cs
+++ Algorithms/Algorithms/Strings/StringsSum/StringsSum.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algorithms.Arithmetical.StringsSum
+namespace Algorithms.Strings.StringsSum
 {
     class StringsSum
     {
