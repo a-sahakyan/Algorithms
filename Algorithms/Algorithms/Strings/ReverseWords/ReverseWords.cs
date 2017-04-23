2a052e30a0a2da3c94b30a49d47a3421b0e19126
arman <arman.sahakyan.dev@gmail.com>
2017-04-24 00:24:07 +0400

    Added ReverseWords task.

---
+++ Algorithms/Algorithms/Strings/ReverseWords/ReverseWords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Strings.ReverseWords
+{
+    class ReverseWords
+    {
+        public void Reverse()
+        {
+            string text = "C# is not C++ and PHP is not Delphi";
+
+            string[] words = text.Split(' ');
+
+            for (int i = 0; i < words.Length / 2; i++)
+            {
+                string temp = words[i];
+                words[i] = words[words.Length - i - 1];
+                words[words.Length - i - 1] = temp;
+            }
+
+            Print(words);
+        }
+
+        private void Print(string[] words)
+        {
+            StringBuilder build = new StringBuilder();
+
+            string result = null;
+            for (int i = 0; i < words.Length; i++)
+            {
+                result = build.Append(words[i]).Append(" ").ToString();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
