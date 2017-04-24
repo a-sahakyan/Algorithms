7a1f3fbcef677ed0558076e93ed790752e1065be
arman <arman.sahakyan.dev@gmail.com>
2017-04-25 01:45:21 +0400

    Changed path of Palindrome.

---
+++ Algorithms/Algorithms/Strings/Palindrome/Palindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Strings.Palindrome
+{
+    class Palindrome
+    {
+        public bool IsPalindrome(string str)
+        {
+            bool isPalindrome = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == str[str.Length - i - 1])
+                {
+                    isPalindrome = true;
+                }
+                else
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            return isPalindrome;
+        }
+    }
+}
