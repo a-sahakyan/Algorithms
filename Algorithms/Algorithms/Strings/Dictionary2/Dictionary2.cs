1c1749f3423398c6d1726860d14cb19c9f22e8e5
arman <arman.sahakyan.dev@gmail.com>
2017-04-27 21:00:44 +0400

    Added Dictionary2 task.

---
+++ Algorithms/Algorithms/Strings/Dictionary2/Dictionary2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algorithms.Strings.Dictionary2
+{
+    class Dictionary2
+    {
+        private string[] key = new string[5];
+        private string[] value = new string[5];
+
+        public Dictionary2()
+        {
+            key[0] = ".NET"; value[0] = "platform for applications from Microsoft";
+            key[1] = "CLR"; value[1] = "managed execution environment for .NET";
+            key[2] = "namespace"; value[2] = "hierarchical organization of classes";
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < key.Length; i++)
+                {
+                    if (key[i] == index)
+                        return $"{key[i]} - {value[i]}";
+                }
+                return "There aren't such a word";
+            }
+        }
+
+        public void Search()
+        {
+            while (true)
+            {
+                Dictionary2 dictionary = new Dictionary2();
+                string key = Console.ReadLine();
+                Console.WriteLine($"{dictionary[key]}");
+            }
+        }
+    }
+}
