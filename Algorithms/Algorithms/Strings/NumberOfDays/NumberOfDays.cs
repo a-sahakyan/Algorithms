2ee9aafbf0415a08eca55912d41452ce57a2e145
arman <arman.sahakyan.dev@gmail.com>
2017-04-27 20:29:36 +0400

    Added NumberOfDays task.

---
+++ Algorithms/Algorithms/Strings/NumberOfDays/NumberOfDays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Strings.NumberOfDays
+{
+    class NumberOfDays
+    {
+        public void Calculate()
+        {
+            while (true)
+            {
+                Console.Write("Enter the first date: ");
+                string date = Console.ReadLine();
+                Console.Write("Enter the second date:");
+                string date2 = Console.ReadLine();
+                DateTime dateTime = DateTime.ParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None);
+                DateTime dateTime2 = DateTime.ParseExact(date2, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None);
+
+                Console.WriteLine($"Distance: {dateTime - dateTime2} days");
+            }
+        }
+    }
+}
